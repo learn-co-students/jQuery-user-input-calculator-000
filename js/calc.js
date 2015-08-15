d456542955f5841e124d9cb639558f98c83bf99c
mbrickenworthe <adamwinters456@gmail.com>
2015-08-15 19:43:46 -0400

    Creating jQuery calculator

--- js/calc.js
+++ js/calc.js
@@ -5,4 +5,26 @@ $(function(){
 
 });
 
-// your code here!
+function Calculator() {
+
+  $('#equals').click(function(){
+  var operation = $('#operation').val()
+  var number1 =   $('#number1').val()
+  var number2 =   $('#number2').val()
+
+    if ((parseInt(number1) * 0 ) != 0 || (parseInt(number2) * 0 ) != 0 ) {
+      $('#result').text("Sorry, one of those is not a valid number!")
+    } else if (operation == '-') {
+      $('#result').text(parseInt(number1) - parseInt(number2))
+    } else if (operation == '+') {
+      $('#result').text(parseInt(number1) + parseInt(number2))
+    } else if (operation == '*') {
+      $('#result').text(parseInt(number1) * parseInt(number2))
+    } else if (operation == '/') {
+      $('#result').text(parseInt(number1) / parseInt(number2))
+    } else
+      $('#result').text("Sorry, not a valid operation!")
+
+  });
+
+}
